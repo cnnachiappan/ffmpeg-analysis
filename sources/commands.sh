90a35ea7564f07fffad33c05562058c3d9884c1f
gemdroid <gemdroid.psu@gamil.com>
2015-09-25 17:37:59 -0400

    added source

---
+++ sources/commands.sh
@@ -0,0 +1,16 @@
+ 2033  ./bin/tutorial01.out hst_2.mpg 
+ 2034  ll
+ 2035  vim tutorial01.c 
+ 2036  make
+ 2037  ./bin/tutorial01.out hst_2.mpg 
+ 2038  vim tutorial01.c 
+ 2039  ./bin/tutorial01.out Marvel\'s\ Avengers-\ Age\ of\ Ultron\ -\ Trailer\ 3.mp4
+ 2040  vim tutorial01.c 
+ 2041  make
+ 2042  ./bin/tutorial01.out Marvel\'s\ Avengers-\ Age\ of\ Ultron\ -\ Trailer\ 3.mp4
+ 2043  ffplay -debug vis_mb_type -vismv 7 Marvel\'s\ Avengers-\ Age\ of\ Ultron\ -\ Trailer\ 3.mp4 
+ 2044  ffplay -debug vis_mb_type 7 Marvel\'s\ Avengers-\ Age\ of\ Ultron\ -\ Trailer\ 3.mp4 
+ 2045  ffplay -debug vis_mb_type Marvel\'s\ Avengers-\ Age\ of\ Ultron\ -\ Trailer\ 3.mp4 
+ 2046  ffmpeg -debug vis_mb_type -i Marvel\'s\ Avengers-\ Age\ of\ Ultron\ -\ Trailer\ 3.mp4 output.mp4
+ 2047  history
+
